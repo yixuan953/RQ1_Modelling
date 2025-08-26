c862d4596e08d67fc3ed0d96d4b7295491e10f0f
yixuan953 <zhou111@node270.internal.anunna.wur.nl>
2025-08-26 12:21:41 +0200

    Revise

--- S1_Yp_IrrigationDemand/WOFOST_rice.sh
+++ S1_Yp_IrrigationDemand/WOFOST_rice.sh
@@ -19,22 +19,22 @@ echo "Current directory: $(pwd)"
 
 # Maize Maximum CycleLength = 160
 # ./wofost-S1-maize list_Rhine_maize.txt meteolist_WFDE5_Rhine_maize.txt 
-# ./wofost-S1-maize list_LaPlata_maize.txt meteolist_WFDE5_LaPlata_maize.txt 
+./wofost-S1-maize list_LaPlata_maize.txt meteolist_WFDE5_LaPlata_maize.txt 
 # ./wofost-S1-maize list_Yangtze_maize.txt meteolist_WFDE5_Yangtze_maize.txt 
 # ./wofost-S1-maize list_Indus_maize.txt meteolist_WFDE5_Indus_maize.txt 
 
 # Rice Maximum CycleLength = 180
-./wofost-S1-rice list_Indus_mainrice.txt meteolist_WFDE5_Indus_mainrice.txt 
-# ./wofost-S1-rice list_LaPlata_mainrice.txt meteolist_WFDE5_LaPlata_mainrice.txt 
+# ./wofost-S1-rice list_Indus_mainrice.txt meteolist_WFDE5_Indus_mainrice.txt 
+./wofost-S1-rice list_LaPlata_mainrice.txt meteolist_WFDE5_LaPlata_mainrice.txt 
 # ./wofost-S1-rice list_Yangtze_mainrice.txt meteolist_WFDE5_Yangtze_mainrice.txt 
 # ./wofost-S1-rice list_Yangtze_secondrice.txt meteolist_WFDE5_Yangtze_secondrice.txt 
 
 # Wheat Maximum CycleLength = 270
 # ./wofost-S1-winterwheat list_Rhine_wheat.txt meteolist_WFDE5_Rhine_wheat.txt 
-# ./wofost-S1-winterwheat list_LaPlata_wheat.txt meteolist_WFDE5_LaPlata_wheat.txt 
+./wofost-S1-winterwheat list_LaPlata_wheat.txt meteolist_WFDE5_LaPlata_wheat.txt 
 # ./wofost-S1-winterwheat list_Yangtze_wheat.txt meteolist_WFDE5_Yangtze_wheat.txt 
 # ./wofost-S1-winterwheat list_Indus_wheat.txt meteolist_WFDE5_Indus_wheat.txt 
 
 # Soybean CycleLength = 150
 # ./wofost-S1-soybean list_Yangtze_soybean.txt meteolist_WFDE5_Yangtze_soybean.txt 
-# ./wofost-S1-soybean list_LaPlata_soybean.txt meteolist_WFDE5_LaPlata_soybean.txt 
+./wofost-S1-soybean list_LaPlata_soybean.txt meteolist_WFDE5_LaPlata_soybean.txt 
